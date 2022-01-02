6131a1f09494d8523a63b5502d5528cb0da775b1
mdAshik922 <md.ashik0005723@gmail.com>
2022-01-02 21:58:24 +0600

    commit

--- src/component/ServiceDetails/ServiceDetail.js
+++ src/component/ServiceDetails/ServiceDetail.js
@@ -3,10 +3,9 @@ import {useParams} from "react-router-dom";
 import Fade from 'react-reveal/Fade';
 
 const ServiceDetail = () => { 
-
-    const {id}=useParams()
+    const {id} = useParams()
     
-    const [specificDetail,setSpecificDetail] = useState({})
+    const [specificDetail, setSpecificDetail] = useState({})
 
 
   useEffect(() =>
@@ -18,18 +17,11 @@ const ServiceDetail = () => {
   })
 ,[])
 
-
- 
-        
-
-
-
     return (
         <div>
             <h2>Please Order now{id}</h2>
              <div  >
              
-             <h2> This is dynamic page and id is:{id}</h2>
            <p>Name: {specificDetail?.about}</p>
            <p>price: {specificDetail?.price}</p>
              
