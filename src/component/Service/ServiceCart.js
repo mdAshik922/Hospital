8771beffa8fbebb2da39b66f3a3eadbf41d0dcfe
mdAshik922 <md.ashik0005723@gmail.com>
2022-01-26 10:40:29 +0600

    commit

--- src/component/Service/ServiceCart.js
+++ src/component/Service/ServiceCart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Fade from 'react-reveal/Fade';
 import './ServiceCart.css';
 
 const ServiceCart = ({service}) => {
@@ -11,7 +11,11 @@ const ServiceCart = ({service}) => {
             <div className="container-style" >
            <img className="img-style" src={picture} alt="health"/>
            <h2>Name:{name}</h2>
-            <p><strong>{about.slice(0, 112)}<Link to={`/ServiceDetail/${id}`}>Learn-more</Link></strong></p>
+           
+            <p><strong>{about.slice(0, 112)}</strong></p>
+            <Link to={`/ServiceDetail/${id}`}>
+           <button style={{backgroundColor: "blue", color: "white"}}>Learn-more....</button>
+            </Link>
             </div>
         </div>
     );
