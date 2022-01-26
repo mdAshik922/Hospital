ae994f010312fb9b3d1176e223336c9f38bd3706
mdAshik922 <md.ashik0005723@gmail.com>
2022-01-26 10:18:47 +0600

    commit

--- src/component/ServiceDetails/ServiceDetail.js
+++ src/component/ServiceDetails/ServiceDetail.js
@@ -3,14 +3,14 @@ import {useParams} from "react-router-dom";
 import Fade from 'react-reveal/Fade';
 
 const ServiceDetail = () => { 
-    const {id} = useParams();
-    const [specificDetail, setSpecificDetail] = useState({});
+    const { id } = useParams();
+    const [ specificDetail, setSpecificDetail ] = useState({});
 
   useEffect(() =>
   fetch("/FakeData.json")
   .then(res => res.json())
   .then(data=>{
-    const matchedData= data.find(detail=> detail.id==id)
+    const matchedData= data.find(detail=> detail.id == id)
     setSpecificDetail(matchedData);})
     ,[id]);
   
